7b35b1ca314c9aa9943336f8a1ee68881d15f151
King Goosey <kinggoosey@gmail.com>
2019-02-11 22:30:09 -0800

    Configured pourer bios to slide up when hovering over pourer.  Added color and shadows to pourer section. Adjusted page font and size.

---
+++ js/pourers.js
@@ -0,0 +1,30 @@
+//load JSON and populate pourers
+$.getJSON('pourers.json')
+    .done(function (data) {
+        $.each(data, function (r, pourer) {
+            var card = "",
+                modal = "",
+                hyphenName = pourer.name.replace(' ', '-').replace(' ', '-');
+
+            card += '<div class="card mco-card shadow">';
+            card += '<div class="mco-img">';
+            card += '<img src="./img/pourers/' + hyphenName + '-thumb.jpg" class="card-img-top">';
+            if (pourer.bio) {
+                card += '<div class="mco-bio">';
+                pourer.bio.forEach(function (paragraph) {
+                    card += '<p>';
+                    card += paragraph;
+                    card += '</p>';
+                });
+                card += '</div>';
+            }
+            card += '</div>';
+            card += '<div class="card-body  bg-purple">';
+            card += '<h5 class="card-title">' + pourer.name + '</h5>';
+            card += '</div>';
+            card += '</div>';
+
+            $('#pourer-deck').append(card);
+
+        });
+    });
